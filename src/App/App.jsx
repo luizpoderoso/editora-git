23f74b508917ac6b5fc846e0db12f4d8e03fc251
luizpoderoso <luizpoderoso72@outlook.com>
2023-07-27 22:30:50 -0300

    Some css stuff.
    Added some stylization.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from "react-router";
 import Navigation from "./components/Navigation";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>
-
+    <div className="w-screen h-screen flex flex-col bg-zinc-100">
       <Navigation />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<h1>Início</h1>} />
-          <Route path="/catalogo" element={<h1>Catálogo</h1>} />
-          <Route path="/livros/:isbn" element={<h1>Página de Livro</h1>} />
-          <Route path="/categoria/:category" element={<h1>Página de Livro</h1>} />
-        </Routes>
+      <main className="px-32">
+        <hr className="border-zinc-400"></hr>
+
+        <div className="py-4 px-2">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogo" element={<h1>Catálogo</h1>} />
+            <Route path="/livros/:isbn" element={<h1>Página de Livro</h1>} />
+            <Route path="/categoria/:category" element={<h1>Categoria</h1>} />
+          </Routes>
+        </div>
       </main>
 
     </div>
--- src/App/components/Navigation.jsx
+++ src/App/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => (
-  <nav>
-    <NavLink className={({ isActive }) => isActive ? 'text-red-500' : 'transition ease-in-out hover:text-zinc-500'} to='/'>Início</NavLink>
-    <NavLink className={({ isActive }) => isActive ? 'text-red-500' : 'transition ease-in-out hover:text-zinc-500'} to='/catalogo'>Catálogo</NavLink>
-  </nav>
-)
+const Navigation = () => {
+  const texts = require('../../api/texts.json');
+  const cssNavLink = (bool) => bool ? 'text-red-600' : 'transition ease-in-out hover:text-zinc-500'; // return the corret className to NavLink
+
+  return (
+    <nav className="w-full inline-flex items-center justify-center p-4 space-x-10 text-xl">
+      <NavLink className={({ isActive }) => cssNavLink(isActive)} to='/'>Início</NavLink>
+      <h1 className="select-none text-5xl">{texts.title}</h1>
+      <NavLink className={({ isActive }) => cssNavLink(isActive)} to='/catalogo'>Catálogo</NavLink>
+    </nav>
+  );
+};
 
 export default Navigation;
