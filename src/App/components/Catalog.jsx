712a1e4b9a5fe80f8b780a70f0589b7e3c97db61
luizpoderoso <luizpoderoso72@outlook.com>
2023-07-30 11:21:54 -0300

    Basic dark mode

--- src/App/components/Catalog.jsx
+++ src/App/components/Catalog.jsx
@@ -33,13 +33,13 @@ const Catalog = ({books, texts}) => {
         {size.width >= 768 && <h1 className='w-fit text-4xl 2xl:text-5xl'>{texts['catalog-title']}</h1>}
         <div className='md:absolute flex md:mt-4 right-0'>
           <label htmlFor='form' className='2xl:text-xl 2xl:mt-1 font-medium mr-2'>{texts['search-label']}</label>
-          <form onSubmit={(event) => handleSubmit(event, setList)} name='form' className='overflow-hidden border rounded-lg text-base xl:text-xl 2xl:text-3xl bg-zinc-50 border-zinc-200'>
-            <input onChange={e => setSearch(e.target.value)} required value={search} className='px-1 rounded-l-lg appearance-none' />
-            <button type='submit' className='px-1 rounded-r-lg text-zinc-400 bg-white cursor-pointer'>&#10095;</button>
+          <form onSubmit={(event) => handleSubmit(event, setList)} name='form' className='overflow-hidden border rounded-lg text-base xl:text-xl 2xl:text-3xl bg-zinc-50 border-zinc-200 dark:bg-zinc-800 dark:border-zinc-900'>
+            <input onChange={e => setSearch(e.target.value)} required value={search} className='px-1 rounded-l-lg appearance-none dark:bg-zinc-800' />
+            <button type='submit' className='px-1 rounded-r-lg text-zinc-400 bg-white cursor-pointer dark:bg-zinc-800'>&#10095;</button>
           </form>
         </div>
       </div>
-
+      <hr className='mt-4 border-zinc-900'></hr>
       <ul className='my-5 w-full md:w-fit h-full grid justify-center sm:grid-cols-3 md:grid-cols-none lg:grid-cols-2 3xl:grid-cols-3 lg:gap-x-3 xl:gap-x-2 1.5xl:gap-x-14 2xl:gap-x-24 3xl:gap-x-14 gap-y-10 lg:gap-y-10 xl:gap-y-20 overflow-y-auto no-scroll overflow-x-hidden'>
         {list}
       </ul>
